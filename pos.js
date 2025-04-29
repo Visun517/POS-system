2e9d57df934610d92d12a2c06b87dcb5f2bd5986
Visun517 <visunpraboda999@gmail.com>
2025-04-29 09:37:48 +0530

    navigate

---
+++ pos.js
@@ -0,0 +1,48 @@
+// document.getElementById('home').style.display = 'block';
+// document.getElementById('customer').style.display = 'none';
+// document.getElementById('item').style.display = 'none';
+// document.getElementById('placeOrder').style.display = 'none';
+
+
+$('#home').css('display', 'block');
+$('#customer').css('display', 'none');
+$('#item').css('display', 'none');
+$('#placeOrder').css('display', 'none');
+
+
+$('#home-btn').on('click', function(){
+
+    $('#home').css('display', 'block');
+    $('#customer').css('display', 'none');
+    $('#item').css('display', 'none');
+    $('#placeOrder').css('display', 'none');
+
+});
+
+$('#customer-btn').on('click', function(){
+
+    $('#home').css('display', 'none');
+    $('#customer').css('display', 'block');
+    $('#item').css('display', 'none');
+    $('#placeOrder').css('display', 'none');
+
+});
+
+$('#item-btn').on('click', function(){
+
+    $('#home').css('display', 'none');
+    $('#customer').css('display', 'none');
+    $('#item').css('display', 'block');
+    $('#placeOrder').css('display', 'none');
+
+});
+
+$('#place-order-btn').on('click', function(){
+
+    $('#home').css('display', 'none');
+    $('#customer').css('display', 'none');
+    $('#item').css('display', 'none');
+    $('#placeOrder').css('display', 'block');
+
+});
+
